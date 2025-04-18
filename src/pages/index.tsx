8c646ef22847ef70ee8c21a900091e943f9f3e18
Hosse Amro <hosse.amro@gmail.com>
2025-04-19 01:24:15 +0330

    mirror

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
-import { TryCatchUTIL } from '@utils';
-
 export const Home = () => {
-	function add(a: number, b: number) {
-		return a + b;
-	}
-
-	const result = TryCatchUTIL.try(add, [10, 5], console.error);
-	console.log('result', result);
-
 	return (
 		<>
-			<div>Test TryCatchUTIL</div>
+			<div>Why</div>
 		</>
 	);
 };
